e3113c2512c8290fe8a160398cf91b0e0b79a3d1
JS702 <83334396+JS702@users.noreply.github.com>
2022-10-08 18:36:40 +0200

    init

---
+++ src/main/java/com/example/ProjektB/ProjektBApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjektB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjektB/ProjektBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjektB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
