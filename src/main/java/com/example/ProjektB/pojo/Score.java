24ca02cc836edaa7c7b62c6b1388b404d90c75a4
Jon Steinkamp <steinkamp@moovin.de>
2022-12-07 19:20:54 +0100

    changed score

--- src/main/java/com/example/ProjektB/pojo/Score.java
+++ src/main/java/com/example/ProjektB/pojo/Score.java
@@ -1,17 +1,14 @@
 package com.example.ProjektB.pojo;
 
-import com.example.ProjektB.domainvalue.GameMode;
 import lombok.Data;
 
 @Data
 public class Score {
 
-    private GameMode gameMode;
-
     private Integer score;
 
-    private Float averageDistance;
+    private Integer averageDistance;
 
-    private Float averageTime;
+    private Integer minimalDistance;
 
 }
